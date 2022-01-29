3d098c9a181bfa45189fd2ad604ac593792d2d11
Steve Rose <stevenohr21.github@gmail.com>
2022-01-29 14:15:47 +1000

    initial commit

---
+++ gatsby/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Nav() {
+	return (
+		<nav>
+			<li><Link to="/">Home</Link></li>
+			<li><Link to="/beers">Beers</Link></li>
+			<li><Link to="/pizzas">Pizzas</Link></li>
+            <li><Link to="/order">Order</Link></li>
+            <li><Link to="/slicemasters">Slicemasters</Link></li>
+		</nav>
+	);
+}
